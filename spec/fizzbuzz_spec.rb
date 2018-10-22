ee3b825cb60698261c27c20ff8e3e4a51f35be78
will mcbrien <williamjamesmcbrien@gmail.com>
2018-10-22 17:33:32 +0100

    Add spec and condition for numbers not divisible by three or five

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,17 +1,21 @@
 require_relative '../lib/fizzbuzz'
 describe 'fizzbuzz' do
 it 'returns "fizzbuzz" when number is divisible by both 3 and 5' do
-  number = 15 * (rand * 100).floor
+  number = 15
   expect(fizzbuzz(number)).to eq 'fizzbuzz'
-end  
+end
 it 'returns "fizz" when number is divisible by 3' do
-    number = 3 * (rand * 100).floor
+    number = 33
     expect(fizzbuzz(number)).to eq 'fizz'
   end
 
   it 'returns "buzz" when number is divisible by 5' do
-    number = 5 * (rand * 100).floor
+    number = 95
     expect(fizzbuzz(number)).to eq 'buzz'
   end
 
+  it 'returns the number if it is not divisible by either 5 of three' do
+    number = 71
+    expect(fizzbuzz(number)).to eq '71'
+  end
 end
