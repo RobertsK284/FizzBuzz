19aee1fc66e1372df0f723766b6623ab0d110fa0
will mcbrien <williamjamesmcbrien@gmail.com>
2018-10-22 17:12:59 +0100

    Add more robust test for 'buzz'cases

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,4 @@
 def fizzbuzz(number)
+  return "buzz" if number % 5 == 0
   return "fizz" if number == 3
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when number is divisible by 5' do
+    number = 5 * (rand * 100).floor
+    expect(fizzbuzz(number)).to eq 'buzz'
+  end
+
 end
