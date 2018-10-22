360fbd045e4a1e7f4b8ff406f4992b95eddf4768
Karen Roberts <lanxuin@gmail.com>
2018-10-22 16:24:57 +0000

    Rspec test and code for fizzbuzz cases added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,12 @@
 require_relative '../lib/fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+it 'returns "fizzbuzz" when number is divisible by both 3 and 5' do
+  number = 15 * (rand * 100).floor
+  expect(fizzbuzz(number)).to eq 'fizzbuzz'
+end  
+it 'returns "fizz" when number is divisible by 3' do
+    number = 3 * (rand * 100).floor
+    expect(fizzbuzz(number)).to eq 'fizz'
   end
 
   it 'returns "buzz" when number is divisible by 5' do
